229919676d581857b568d452b67ca577fa810e7d
shivamverma29 <shivamverma2908@gmail.com>
2023-08-09 11:40:05 +0530

    changes in cart

--- src/Components/item.js
+++ src/Components/item.js
@@ -1,32 +1,80 @@
-import React, { useContext,useState,useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import image from "../images/item.png";
 import { AiOutlinePlus } from "react-icons/ai";
 import { AiOutlineMinus } from "react-icons/ai";
 import "./CSS/Item.css";
-import {Cart} from '../Context/cart'
+import { Cart } from "../Context/cart";
 // import {car} from './ARShop'
 
 const Item = (props) => {
-  const {cartItems, setCart} = useContext(Cart);
+  const { cartItems, setCart } = useContext(Cart);
   const Addbtn = (event) => {
     document.getElementById(event.target.id).style.display = "none";
     document.getElementById(`a${event.target.id}`).style.display = "block";
-    cartItems[props.data.id]=1
-    setCart({1:cartItems[1],2:cartItems[2],3:cartItems[3],4:cartItems[4],5:cartItems[5],6:cartItems[6],7:cartItems[7],8:cartItems[8],9:cartItems[9],10:cartItems[10],11:cartItems[11],12:cartItems[12]});
+    cartItems[props.data.id] = 1;
+    setCart({
+      1: cartItems[1],
+      2: cartItems[2],
+      3: cartItems[3],
+      4: cartItems[4],
+      5: cartItems[5],
+      6: cartItems[6],
+      7: cartItems[7],
+      8: cartItems[8],
+      9: cartItems[9],
+      10: cartItems[10],
+      11: cartItems[11],
+      12: cartItems[12],
+    });
   };
   const addVal = (event) => {
-    cartItems[props.data.id]+=1
-    console.log(cartItems)
-    setCart({1:cartItems[1],2:cartItems[2],3:cartItems[3],4:cartItems[4],5:cartItems[5],6:cartItems[6],7:cartItems[7],8:cartItems[8],9:cartItems[9],10:cartItems[10],11:cartItems[11],12:cartItems[12]});
+    cartItems[props.data.id] += 1;
+    console.log(cartItems);
+    setCart({
+      1: cartItems[1],
+      2: cartItems[2],
+      3: cartItems[3],
+      4: cartItems[4],
+      5: cartItems[5],
+      6: cartItems[6],
+      7: cartItems[7],
+      8: cartItems[8],
+      9: cartItems[9],
+      10: cartItems[10],
+      11: cartItems[11],
+      12: cartItems[12],
+    });
   };
   const subVal = (event) => {
-    cartItems[props.data.id] -= 1;
-    setCart({1:cartItems[1],2:cartItems[2],3:cartItems[3],4:cartItems[4],5:cartItems[5],6:cartItems[6],7:cartItems[7],8:cartItems[8],9:cartItems[9],10:cartItems[10],11:cartItems[11],12:cartItems[12]});
+    if (cartItems[props.data.id]) {
+      cartItems[props.data.id] -= 1;
+    }
+    // else if (cartItems[props.data.id] == 0) {
+    //   document.getElementById(event.target.id).style.display = "block";
+    //   document.getElementById(`a${event.target.id}`).style.display = "none";
+    // }
+    setCart({
+      1: cartItems[1],
+      2: cartItems[2],
+      3: cartItems[3],
+      4: cartItems[4],
+      5: cartItems[5],
+      6: cartItems[6],
+      7: cartItems[7],
+      8: cartItems[8],
+      9: cartItems[9],
+      10: cartItems[10],
+      11: cartItems[11],
+      12: cartItems[12],
+    });
   };
+  const data = Array.from(props.data);
   return (
     <div class="card-wrapper">
+      {/* {data.map((item) => ( */}
       <div class="card">
         <img src={props.data.Img} class="h-80 card-img-top" alt="..." />
+        {console.log(4)}
         <div class="card-body">
           <h5 class="card-title font-bold">{props.data.name}</h5>
           <p class="card-text">Price:{props.data.price}</p>
@@ -39,7 +87,11 @@ const Item = (props) => {
           >
             Add to cart
           </button>
-          <div className="mt-2" id={`aaddcart${props.data.id}`} style={{ display: "none" }}>
+          <div
+            className="mt-2"
+            id={`aaddcart${props.data.id}`}
+            style={{ display: "none" }}
+          >
             <button className="btn btn-primary mr-1">
               <AiOutlineMinus onClick={subVal} />
             </button>
@@ -51,6 +103,7 @@ const Item = (props) => {
         </div>
         {/* <a href="#" class="btn btn-primary">AR</a> */}
       </div>
+      {/* ))} */}
     </div>
   );
 };
